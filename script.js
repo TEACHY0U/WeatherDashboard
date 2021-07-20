3ee6cd0d4beb92647ff2e234b7174ab1208da9ec
TeachY0u <marianoteach@rocketmail.com>
2021-07-20 15:42:31 -0700

    adding scripts

--- script.js
+++ script.js
@@ -1,14 +1,24 @@
 var button = document.querySelector('.button');
 var inputValue = document.querySelector('.inputValue');
-var name = document.querySelector('.name');
+var cityName = document.querySelector('.name');
 var desc = document.querySelector('.desc');
 var temp = document.querySelector('.temp');
 
 
 button.addEventListener('click',function(){
-    fetch('api.openweathermap.org/data/2.5/weather?q='+inputValue.value+'&appid=8493b22548e64a9d9ddf5977b11abe02')
+    // fetch('api.openweathermap.org/data/2.5/weather?q='+inputValue.value+'&appid=8493b22548e64a9d9ddf5977b11abe02')
+    fetch('https://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=8493b22548e64a9d9ddf5977b11abe02&units=imperial')
 .then(response => response.json())
-.then(data => console.log(data))
+.then(data => {
+    var nameValue = data['name'];
+    var tempValue = data['main']['temp'];
+    var descValue = data['weather'][0]['description'];
 
-.catch(err => alert("wrong city name!"))
+    name.innerHTML = nameValue;
+    temp.innerHTML = tempValue;
+    desc.innerHTML = descValue;
+})
+
+
+.catch(err => alert("Wrong city name!"))
 })
