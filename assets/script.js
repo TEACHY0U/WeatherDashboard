251808833cf9e21aeff7099b0fa3c6422df4d65b
TeachY0u <marianoteach@rocketmail.com>
2021-11-06 13:25:04 -0700

    adding scripts

---
+++ assets/script.js
@@ -0,0 +1,8 @@
+$(document).ready(function () {
+  let appID = "8493b22548e64a9d9ddf5977b11abe02";
+  let weather = "";
+  let city = "";
+  let current_date = moment().format("L");
+  let search_history =
+ 
+});
